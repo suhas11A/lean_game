5bbd167741a0e336a894088323af9a065dde31dc
suhas <suhas1alladaboina@gmail.com>
2025-06-03 01:11:34 +0000

    Added first level  for sets

---
+++ Game/Levels/Sets.lean
@@ -0,0 +1,21 @@
+import Game.Levels.Sets.L01_intro
+
+World "Sets"
+Title "Chapter 2: Sets"
+
+Introduction "
+Welcome to Set World!
+
+The elements of the sets in this level will come from a universe called `U`.
+To specify that an object `x` belongs to the universe `U`, we write `x : U`.
+To specify that `A` is a set of objects from `U`, we write `A : Set U`.
+(The terminology used in Lean is that `x` has type `U` and `A` has type `Set U`.)
+To say that `x` is an element of `A`, we write `x ∈ A`.
+(You can enter the symbol `∈` by typing `\\mem` or `\\in`.)
+
+
+The notation A ⊆ B means that A is a subset of B.
+(To enter the symbol ⊆, type `\\sub`, followed by a space.)
+
+Click on 'Start' below to get started.
+"
---
+++ Game/Levels/Sets/L01_intro.lean
@@ -0,0 +1,29 @@
+import Game.Metadata
+import Mathlib.Data.Set.Basic
+
+World "Sets"
+Level 1
+Title "Introduction to Subsets"
+
+Introduction "
+Following along with Example 2.1.16, let's prove a basic set theorem using Lean.
+
+To prove A ⊆ B, we need to show that if something is an element of A, then it is also an element of B i.e we
+need to prove `x ∈ A → x ∈ B`
+(To enter the symbol ⊆ type `\\sub`, and for ∈ type `\\mem` or `\\in`).
+
+To prove tha goal the first tactic you're going to use is `intro`. Type in `intro x hx` to get started, this will introduce a new
+hypotheses named `hx` with new variable `x` that is needed to prove the goal. Read the documentation on the right by clicking on `intro`.
+Alternatively you could execute `intro x` to first introduce a new variable `x` into goal and then introduce a new hypotheses.
+"
+
+/-- Suppose A is a set. Then A⊆A. -/
+Statement (U : Type)(A : Set U): A ⊆ A := by
+  intro x hx
+  Hint "Now that we have a goal that matches one of the hypotheses. We use `exact` tactic to close the goal. Read the documentation on the right by clicking on `exact`."
+  exact hx
+
+Conclusion "
+Congratulations! You have completed your first proof in Sets using LEAN!
+"
+NewTactic exact
