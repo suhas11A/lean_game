d623d54a16210a44cba5a8a9e8454710802885c0
Rohan Bafna <130247393+rohanbafna@users.noreply.github.com>
2025-06-13 16:42:54 -0400

    And_elim tactic

--- Game/Tactic.lean
+++ Game/Tactic.lean
@@ -2,12 +2,11 @@
 
 import Lean
 
-open Lean.Elab.Tactic Lean.Meta
+open Lean.Elab.Tactic Lean.Meta Lean.MonadLCtx
 
 -- Proceeds with and-introduction.  Works like apply And.intro.
 elab "and_intro" : tactic =>
-  withMainContext do
-    let goal ← getMainGoal
+  withMainContext $ liftMetaTactic λ goal ↦ do
     let decl ← goal.getDecl
     -- Convert the goal to WHNF to compare.
     let whnfGoal ← whnf decl.type
@@ -19,9 +18,9 @@ elab "and_intro" : tactic =>
       -- Close the current goal with And.intro, and add new goals for
       -- each conjunct.
       goal.assign (← mkAppM ``And.intro #[mvar1, mvar2])
-      replaceMainGoal [Lean.Expr.mvarId! mvar1, Lean.Expr.mvarId! mvar2]
+      pure [Lean.Expr.mvarId! mvar1, Lean.Expr.mvarId! mvar2]
     else
-      Lean.Meta.throwTacticEx `and_intro goal
+      throwTacticEx `and_intro goal
         m!"the goal {decl.type} isn't of the form p ∧ q"
 
 example : (0 = 0 ∧ 2 = 2) ∧ 1 = 1 := by
@@ -30,3 +29,37 @@ example : (0 = 0 ∧ 2 = 2) ∧ 1 = 1 := by
     · rfl
     · rfl
   · rfl
+
+-- Proceeds with and-elimination.  Replaces a hypothesis of the form
+-- p ∧ q with two hypotheses of forms p and q respectively.
+elab "and_elim" h:ident "into" hl:ident hr:ident : tactic =>
+  withMainContext $ liftMetaTactic λ goal ↦ do
+    let ctx ← getLCtx
+    -- Search for a hypothesis with name h.
+    if let some hyp := ctx.findFromUserName? (h.getId) then
+      -- If found, ensure that it is of the form p ∧ q
+      let whnfHyp ← whnf (← inferType hyp.toExpr)
+      if let .app (.app (.const ``And []) conj1) conj2 := whnfHyp then
+        -- Use And's eliminators to get values of type p and q and add
+        -- them to the context.
+        let conj1Val ← mkAppM ``And.left #[hyp.toExpr]
+        let conj2Val ← mkAppM ``And.right #[hyp.toExpr]
+        let ⟨_, goal, _⟩ ← goal.assertAfter hyp.fvarId hr.getId conj2 conj2Val
+        let ⟨_, goal, _⟩ ← goal.assertAfter hyp.fvarId hl.getId conj1 conj1Val
+        -- Clear the original hypothesis as it is not needed anymore.
+        let goal ← goal.clear hyp.fvarId
+        pure [goal]
+      else
+        throwTacticEx `and_elim goal
+          m!"the assumption {h} : {hyp.type} isn't of the form p ∧ q"
+    else
+      throwTacticEx `and_elim goal
+        m!"there is no assumption named {h}"
+
+theorem a (P Q : Prop) (abc : P ∧ Q) : (Q ∧ P) := by
+  and_elim abc into a c
+  and_intro
+  · exact c
+  · exact a
+
+#print a
