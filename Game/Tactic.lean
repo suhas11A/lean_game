a544048f884aff27c85a00a0d947cfcd5b02f33b
Rohan Bafna <130247393+rohanbafna@users.noreply.github.com>
2025-06-13 15:24:46 -0400

    Clean up and_intro

--- Game/Tactic.lean
+++ Game/Tactic.lean
@@ -2,29 +2,31 @@
 
 import Lean
 
+open Lean.Elab.Tactic Lean.Meta
+
 -- Proceeds with and-introduction.  Works like apply And.intro.
 elab "and_intro" : tactic =>
-  Lean.Elab.Tactic.withMainContext do
-    let goal ← Lean.Elab.Tactic.getMainGoal
+  withMainContext do
+    let goal ← getMainGoal
     let decl ← goal.getDecl
-    let error := Lean.Meta.throwTacticEx `and goal m!"the goal {decl.type} isn't of the form p ∧ q"
+    -- Convert the goal to WHNF to compare.
+    let whnfGoal ← whnf decl.type
     -- Check that the goal type is of the form p ∧ q.
-    if let .app (.app and conj1) conj2 := decl.type then
-      if (← Lean.Meta.isDefEq and (.const ``And [])) then
-        -- Add new metavariables for each conjunct.
-        let mvar1 ← Lean.Meta.mkFreshExprMVar conj1
-        let mvar2 ← Lean.Meta.mkFreshExprMVar conj2
-        -- Close the current goal with And.intro, and add new goals for
-        -- each conjunct.
-        Lean.Elab.Tactic.closeMainGoal (Lean.mkApp4 (.const ``And.intro []) conj1 conj2 mvar1 mvar2) false
-        Lean.Elab.Tactic.setGoals ([Lean.Expr.mvarId! mvar1, Lean.Expr.mvarId! mvar2] ++
-                                   (← Lean.Elab.Tactic.getGoals))
-      else
-        error
+    if let .app (.app (.const ``And []) conj1) conj2 := whnfGoal then
+      -- Add new metavariables for each conjunct.
+      let mvar1 ← mkFreshExprMVar conj1
+      let mvar2 ← mkFreshExprMVar conj2
+      -- Close the current goal with And.intro, and add new goals for
+      -- each conjunct.
+      goal.assign (← mkAppM ``And.intro #[mvar1, mvar2])
+      replaceMainGoal [Lean.Expr.mvarId! mvar1, Lean.Expr.mvarId! mvar2]
     else
-      error
+      Lean.Meta.throwTacticEx `and_intro goal
+        m!"the goal {decl.type} isn't of the form p ∧ q"
 
-example : 0 = 0 ∧ 1 = 1 := by
+example : (0 = 0 ∧ 2 = 2) ∧ 1 = 1 := by
   and_intro
-  · rfl
+  · and_intro
+    · rfl
+    · rfl
   · rfl
