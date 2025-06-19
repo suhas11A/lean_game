ae7778b9a3267533d47502da338ce61278fcdbb3
sydneywb <badescusydney@gmail.com>
2025-06-19 14:18:01 -0400

    remove PropLogic levels 7-10

--- Game/Levels/PropositionalLogic.lean
+++ Game/Levels/PropositionalLogic.lean
@@ -4,10 +4,10 @@ import Game.Levels.PropositionalLogic.L03_disjunctHypo
 import Game.Levels.PropositionalLogic.L04_disjunctGoal
 import Game.Levels.PropositionalLogic.L05_impGoal
 import Game.Levels.PropositionalLogic.L06_impHypo
-import Game.Levels.PropositionalLogic.L07_bicondGoal
-import Game.Levels.PropositionalLogic.L08_bicondHypo
-import Game.Levels.PropositionalLogic.L09_negGoal
-import Game.Levels.PropositionalLogic.L10_negHypo
+-- import Game.Levels.PropositionalLogic.L07_bicondGoal
+-- import Game.Levels.PropositionalLogic.L08_bicondHypo
+-- import Game.Levels.PropositionalLogic.L09_negGoal
+-- import Game.Levels.PropositionalLogic.L10_negHypo
 
 World "PropositionalLogic"
 Title "Chapter 1: PropositionalLogic"
