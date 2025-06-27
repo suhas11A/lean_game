65cfd6d92714a1933b44cb5359ffee3deed642e4
Rohan Bafna <130247393+rohanbafna@users.noreply.github.com>
2025-06-27 12:04:26 -0400

    Hints for Variables and Quantifiers level 1
    Slightly reworked the explanation to be clearer on what \forall is and
    how to prove it.

--- Game/Levels/VariablesAndQuantifiers/L01_universalGoal.lean
+++ Game/Levels/VariablesAndQuantifiers/L01_universalGoal.lean
@@ -10,25 +10,47 @@ Title "Universal Quantifier in the Goal"
 
 
 Introduction "
-Let p(x) be a logical formula with free variable x with range X.
-The introduction rule of the universal quantifier (∀) is
+Consider a logical formula $p(x)$ parametrized over a variable $x$
+with domain of discourse $X$.  The formula $∀x∈X, p(x)$ ($∀$ is typed
+as `\\forall` and read as “for all”) expresses that $p(x)$ holds
+whenever $x$ is an element of $X$.  We say that this kind of formula
+is *universally quantified*.
 
-(∀I) If p(x) can be derived from the assumption that x is an arbitrary
-element of X, then ∀x ∈ X, p(x).
+For instance, $∀n∈ℤ, n<n+1$ means that every integer is less than
+itself plus one.  This is obviously true; how do we prove it?
 
-The expression `∀x ∈ X, p(x)` represents \"for all x ∈ X, p(x)\".
-This means that to **prove a goal** of the form `∀x ∈ X, p(x)`, it suffices to take
-an arbitrary y ∈ X and prove p(y) is true.
+To prove a goal of the form $∀x ∈ X, p(x)$, we use the following strategy:
 
-In this level, we have goal `∀x ∈ ℤ, x<x+1`. `forall_intro y` invokes ∀I by taking y to be an
-arbitrary element of ℤ and changing the goal to proving y<y+1 is true. Try using `forall_intro`
-with the correct syntax to begin the proof.
+(∀I) *If $p(x)$ can be derived from the assumption that $x$ is an arbitrary
+element of $X$, then $∀x ∈ X, p(x)$.*
+
+By arbitrary, we mean that we assume nothing about $x$ other than that
+it is an element of $X$.  In particular, we can’t assume that it is
+any *particular* element of $X$.  When we prove $∀n∈ℤ, n<n+1$, we will
+introduce a new integer $n$, but we won't know what $n$ actually is,
+since our proof must work for every possible $n∈ℤ$.
+
+To prove a universally quantified statement in Lean, use the
+`forall_intro` tactic.  Typing `forall_intro y` invokes ∀I by taking
+$y$ to be an arbitrary element of the domain of discourse and changing
+the goal to proving that $p(y)$ is true.  (You can use whatever
+variable name you want instead of `y`.)  Try using `forall_intro` with
+the correct syntax to begin the proof.
 "
 
-Statement : ∀x:ℕ, x<x+1 := by
+/-- For all integers $n$, $n$ is less than $n+1$. -/
+Statement : ∀n:ℤ, n<n+1 := by
   forall_intro y
-  -- TODO: revise proof
-  apply Nat.lt.base y
+  Hint "
+  Observe that `{y}` has been added as a new object, and that the goal
+  has changed to `{y} < {y} + 1`—`n` has been replaced with `{y}`.  To
+  finish the proof, we can use `simp`.
+  "
+  simp
 
 NewTactic forall_intro
-Conclusion ""
+Conclusion "
+If you haven’t noticed, we use the $∈$ symbol (`\\in`, read `in`) with
+$∀$ in convential math notation, but in Lean, we use a colon instead:
+`∀n : ℤ, n < n + 1`.
+"
