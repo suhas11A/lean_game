7825ffc719c3a3474861cf2a9e6249da86e8f5c1
sydneywb <badescusydney@gmail.com>
2025-07-17 11:06:51 -0400

    some more

--- Game/Levels/LogicalEquivalence/L10_exercise.lean
+++ Game/Levels/LogicalEquivalence/L10_exercise.lean
@@ -17,3 +17,24 @@ Introduction ""
 -- tautology like
 -- ∀x ∈ X, [P(x) → Q(x)] ∨ [Q(x) → P(x)]
 -- or (∀x ∈ X, P(x) ∨ Q(x) → ∀x ∈ X, P(x) ∨ ∃x ∈ X, Q(x))
+
+
+/-
+exercise 1.18
+Let X be ℤ or ℚ and define a logical formula p by:
+∀x ∈ X, ∃y ∈ X, (x<y ∧ [∀z ∈ X, ¬(x<z ∧ z<y)])
+Write out ¬p as a maximally negated logical formula.
+Prove that p is true when X = ℤ, and p is false when X = ℚ.
+-/
+
+example : ∀(x:ℤ), ∃(y:ℤ), (x<y ∧ ∀(z:ℤ), ¬(x<z ∧ z<y)) := by
+  intro x
+  use x+1
+  constructor
+  exact lt_add_one x
+  intro z
+  intro h1
+  cases' h1 with h2 h3
+  have h4 : z ≤ x := le_of_lt_add_one h3 -- error here !!!
+  apply not_lt_of_ge at h4
+  contradiction
